85153891ae0d02c67f9dff3617950bdfc68d4816
raryosu <hagi.1007.kosen@gmail.com>
2014-11-13 19:44:15 +0900

    reset

--- raryog/raryog/wsgi.py
+++ raryog/raryog/wsgi.py
@@ -1,14 +1,14 @@
 """
-WSGI config for djangoblog_project project.
+WSGI config for raryog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoblog_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "raryog.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
