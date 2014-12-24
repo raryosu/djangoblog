ac6b46815aa0500bec70c108c6b7b26085df628c
raryosu <hagi.1007.kosen@gmail.com>
2014-12-24 21:39:22 +0900

    いろいろミスったから全部消す

--- raryosublog/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from blog.models import blogdata
-
-# Register your models here.
-class blogdataAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'date',)
-    list_display_links = ('id', 'title',)
-
-admin.site.register(blogdata, blogdataAdmin)
--- raryosublog/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from blog import views
-
-urlpatterns = patterns('',
-        url(r'^$', views.show, name='show'),
-        url(r'^edit/$', views.edit, name='edit'),
-)
