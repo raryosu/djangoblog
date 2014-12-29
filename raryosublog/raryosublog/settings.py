8621c46acd36a1a31f89b103738435aef716ed3c
Ryosuke Hagihara <raryosu@localhost.localdomain>
2014-12-29 22:07:09 +0900

    妥協に妥協を重ねた妥協

--- raryosublog/raryosublog/settings.py
+++ raryosublog/raryosublog/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
     'blog',
 )
 
@@ -82,3 +83,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, "static"),
+)
