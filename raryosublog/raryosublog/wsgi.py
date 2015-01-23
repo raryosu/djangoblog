957d9431e164b5c093d159effd6fe9df5a423587
raryosu <hagi.1007.kosen@gmail.com>
2015-01-24 00:04:31 +0900

    fix for deploy

--- raryosublog/raryosublog/wsgi.py
+++ raryosublog/raryosublog/wsgi.py
@@ -8,7 +8,24 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import site
+import sys
+
+# Virtualenv
+site.addsitedir("/home/raryosu/ENV/lib/python3.3/site-packages")
+
+sys.path.append('/var/www/cgi-bin/raryosublog')
+sys.path.append('/var/www/cgi-bin/raryosublog/raryosublog')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'raryosublog.settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "raryosublog.settings")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+active_env = os.path.expanduser("/home/raryosu/ENV/bin/active_this.py")
+execfile(active_env, dict(__file__=active_env))
+
+
+#from django.core.wsgi import get_wsgi_application
+#application = get_wsgi_application()
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
